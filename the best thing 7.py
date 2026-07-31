e98d6cf66a0f1520b14bf72c438a90e0cf6396d6
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-31 13:31:04 -0700

    Update the best thing 7.py

--- the best thing 7.py
+++ the best thing 7.py
@@ -69,11 +69,5 @@ if st.session_state.num > 0 and st.session_state.num % 10 == 0:
      st.balloons()
      st.session_state.level+=1
      st.session_state.ran+=20
-     del st.session_state.num1
-     del st.session_state.num2
-     del st.session_state.sign
-     st.session_state.count=0
-     st.session_state.num=0
-
 st.write("your points are " ,st.session_state.num,"from",st.session_state.count,"Questions" )
 st.write("you are in level",st.session_state.level)
