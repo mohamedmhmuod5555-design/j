c53a6348c23d65e3868acf5bfffda37f35abe46c
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-31 13:45:24 -0700

    Update the best thing 7.py

--- the best thing 7.py
+++ the best thing 7.py
@@ -20,7 +20,7 @@ if 'num2' not in st.session_state:
 if 'sign' not in st.session_state:
  st.session_state.sign=random.choice(['+','-','*','/'])
 if 'feed' not in st.session_state:
- st.session_state.feed
+ st.session_state.feed=0
 
 num1 = st.session_state.num1
 num2 = st.session_state.num2
