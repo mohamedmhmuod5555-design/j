6e743b5ef554f5210b83ad4e32742bc78e8d270f
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-31 14:09:57 -0700

    Update the best thing 7.py

--- the best thing 7.py
+++ the best thing 7.py
@@ -10,7 +10,7 @@ if 'secret_number' not in st.session_state:
 if 'num' not in st.session_state:
   st.session_state.num=0
 if 'sc' not in st.session_state:
-  st.session_state.sc=0
+  st.session_state.sc
 if 'count' not in st.session_state:
   st.session_state.count=0
 if 'num1' not in st.session_state:
