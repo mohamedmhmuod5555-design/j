72053ab696f426672194f2d31ef35157a1f4826e
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-31 14:26:43 -0700

    Update the best thing 7.py

--- the best thing 7.py
+++ the best thing 7.py
@@ -3,14 +3,14 @@ import time
 import streamlit as st
 if 'ran' not in st.session_state or st.session_state.ran < 1:
   st.session_state.ran=20
-if 'level' not in st.session_state:
-  st.session_state.level=0
 if 'secret_number' not in st.session_state:
-  st.session_state.secret_number=(st.session_state.level*1785/5)//8
+  st.session_state.secret_number=(level*1785/5)//8
+if 'level' not in st.session_state:
+  st.session_state.level=1
 if 'num' not in st.session_state:
   st.session_state.num=0
 if 'sc' not in st.session_state:
-  st.session_state.sc
+  st.session_state.sc=0
 if 'count' not in st.session_state:
   st.session_state.count=0
 if 'num1' not in st.session_state:
@@ -21,8 +21,7 @@ if 'sign' not in st.session_state:
  st.session_state.sign=random.choice(['+','-','*','/'])
 if 'feed' not in st.session_state:
  st.session_state.feed=0
-if 'Checked' not in st.session_state:
- st.session_state.Checked= st.session_state.sc
+
 num1 = st.session_state.num1
 num2 = st.session_state.num2
 sign = st.session_state.sign
@@ -35,43 +34,51 @@ if sign=='*':
 if sign=='/':
  sc=num1//num2 
 st.title("Welcome to Math Game ")
-level_code=st.text_input("ادخل كودك ")
-
 st.write(num1,sign,num2)
-number=st.number_input("ادخل النتيجه ") 
-if number is not None and number !=st.session_state.Checked :
-   st.session_state.Checked = number 
-   st.session_state.count += 1
-   st.session_state.feed="false" 
-  if number == st.session_state.Checked :
-   st.session_state.count += 1
+number=st.number_input("ادخل النتيجه ")
+r=st.empty()
+for i in range(15,-1,-1):
+  st.write("The time Left ",i)
+  time.sleep(1)
+
+if st.button("تأكيد التخمين "):
+  st.session_state.count += 1
+  if number == sc:
    st.session_state.num += 1
    st.session_state.feed="correct"
-   if st.session_state.feed=="correct":
-    st.success("انك اسطوره يا عبقري الرياضه ")
-    st.balloons()
-    st.session_state.feed=None
-    st.session_state.num1=random.randint(1,st.session_state.ran)
-    st.session_state.num2=random.randint(1,st.session_state.ran)
-    st.session_state.sign=random.choice(['+','-','*','/'])
-    time.sleep(1)
-    st.rerun()
   else:
-   st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
-   st.session_state.feed=None
-   st.session_state.num1=random.randint(1,st.session_state.ran)
-   st.session_state.num2=random.randint(1,st.session_state.ran)
-   st.session_state.sign=random.choice(['+','-','*','/'])
-   time.sleep(1)
-   st.rerun()
+   st.session_state.num =0
+   st.session_state.feed="false" 
+if st.session_state.feed=="correct":
+  st.success("انك اسطوره يا عبقري الرياضه ")
+  st.balloons()
+  st.session_state.feed=None
+  st.session_state.num1=random.randint(1,st.session_state.ran)
+  st.session_state.num2=random.randint(1,st.session_state.ran)
+  st.session_state.sign=random.choice(['+','-','*','/'])
+  time.sleep(1)
+  st.rerun()
+if st.session_state.feed=="false":
+  st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
+  st.session_state.feed=None
+  st.session_state.num1=random.randint(1,st.session_state.ran)
+  st.session_state.num2=random.randint(1,st.session_state.ran)
+  st.session_state.sign=random.choice(['+','-','*','/'])
+  time.sleep(1)
+  st.rerun()
 
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
   st.success("انت بطل! تحدي صديقك انه بالطبع لن يستطيع ان يصل لمستواك  ")
   if st.button("الليفل التالي "):
      st.write("Your Code is ",st.session_state.secret_number) 
      st.balloons()
-     
      st.session_state.level+=1
      st.session_state.ran+=20
+     del st.session_state.num1
+     del st.session_state.num2
+     del st.session_state.sign
+     st.session_state.count=0
+     st.session_state.num=0
+
 st.write("your points are " ,st.session_state.num,"from",st.session_state.count,"Questions" )
 st.write("you are in level",st.session_state.level)
