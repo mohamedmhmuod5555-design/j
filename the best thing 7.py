386b150c1d430c0e11089eb8256608f638edb31e
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-31 14:40:04 -0700

    Update the best thing 7.py

--- the best thing 7.py
+++ the best thing 7.py
@@ -37,6 +37,7 @@ number=st.number_input("ادخل النتيجه ")
 r=st.empty()
 for i in range(15,-1,-1):
   r.write(f"The time Left {i}")
+  time.sleep(0.1)
   if i ==0 :
     st.write("للاسف لقد انتهي الوقت ")
     st.session_state.num =0
